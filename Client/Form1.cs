13d0ddbda66bca5288fdd76c933c264cea1b11d9
aedemirsen <aedemirsen@gmail.com>
2017-04-14 17:16:32 +0300

    first commit

---
+++ Client/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        UdpClient udpClient;
+        
+        private string openKey = "qRNhgOv56SDty";
+        private string closeKey = "qRNhgCv56SDty";
+        bool connected = false;
+        public Form1()
+        {
+            InitializeComponent();
+            label4.Text = "Not Connected";
+            textBox1.Text = "localhost";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                udpClient = new UdpClient();
+                udpClient.Connect(textBox1.Text, int.Parse(textBox3.Text));
+                connected = true;
+                label4.Text = "Connected";
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            udpClient.Close();
+            connected = false;
+            label4.Text = "Not Connected";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                Byte[] senddata = Encoding.ASCII.GetBytes(openKey);
+                udpClient.Send(senddata, senddata.Length);
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                Byte[] senddata = Encoding.ASCII.GetBytes(closeKey);
+                udpClient.Send(senddata, senddata.Length);
+            }
+        }
+    }
+}
---
+++ Server/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        bool isRunning = false;
+        string ip;
+        int port;
+        byte control = 0;
+        Thread thdUDPServer;
+        const int index = 5;
+        const string openData = "O";
+        const string closeData = "C";
+
+        public Form1()
+        {
+            InitializeComponent();
+            button2.BackColor = Color.Red;
+        }
+
+        public void serverThread()
+        {
+            UdpClient udpClient = new UdpClient(port);
+            while (true)
+            {
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Parse("195.175.1.1"), 0);
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+                string handledData = getData(returnData,index);
+                if (handledData == openData)
+                {
+                    button2.BackColor = Color.Green;
+                }
+                else if (handledData == closeData)
+                {
+                    button2.BackColor = Color.Red;
+                }
+            }
+        }
+
+        public string getData(string datagram,int index)
+        {
+            string data;
+            data = datagram.ElementAt(index)+"";
+            return data;
+        }
+
+        private void start(object sender, EventArgs e)
+        {
+            if (isRunning == false)
+            {
+                try
+                {
+                    port = int.Parse(textBox2.Text);
+                }
+                catch (Exception ex) { MessageBox.Show("Only numbers are allowed!");return; }
+                if (control == 0)
+                {                    
+                    if (textBox2.Text != "")
+                    {
+                        control = 1;                        
+                        thdUDPServer = new Thread(new
+                        ThreadStart(serverThread));
+                        thdUDPServer.Start();
+                        button1.Text = "Stop Server";
+                        label2.Text = "Server is running...";
+                        isRunning = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Enter a port number!");
+                        return;
+                    }              
+                }
+                else 
+                {
+                    thdUDPServer.Resume();
+                    button1.Text = "Stop Server";
+                    label2.Text = "Server is running...";
+                    isRunning = true;
+                }
+            }            
+            else
+            {
+                button1.Text = "Start Server";
+                label2.Text = "Server is not running...";
+                isRunning = false;
+                thdUDPServer.Suspend();                
+            }
+            
+            
+            
+        }
+    }
+}
